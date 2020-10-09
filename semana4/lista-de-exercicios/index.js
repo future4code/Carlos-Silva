15c9b338e78ec4686ff7825f0c406632823d552b
Augusto Luna <caugustolunas@gmail.com>
2020-10-09 14:05:20 -0300

    Exercicio 1 de interpretação de codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,12 @@
+// *** Exercícios de interpretação de código ***
+
+//Exercício 1
+
+/*
+    Essa função conversorDeMoeda recebe um parametro valorEmDolar, que é um número. O trabalho da função é receber um número informado pelo usuário que será referente à cotação do dolar, e armazenar esse número na const cotação.
+    Depois a função retorna o numero informado pelo usuário multiplicado pelo valorEmDolar, que é o parametro da função.
+    Já fora da função existe uma const meuDinheiro que recebe a função conversorDeMoeda com o parametro 100.
+    Será impresso no console o resultado de 100 * cotação informada pelo usuário.
+    Exemplo (cotação do dolar sendo informada como 5.55): R$ 555
+*/
+
