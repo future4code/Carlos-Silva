449d1f125fcd74c9db6193c555ada0f251359e05
Augusto Luna <caugustolunas@gmail.com>
2020-10-09 14:07:12 -0300

    Exercicio 3 de interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -20,3 +20,16 @@
     165
     TIPO DE INVESTIMENTO INFORMADO INCORRETO
 */
+
+//Exercício 3
+
+/*
+    Esse programa recebe uma array de números chamada numero e duas arrays vazias, chamadas de array1 e array2.
+    Logo abaixo ele roda um loop for of que diz que para cada numero de numeros, ou seja, para cada item da array chamada numeros ele vai analisar se a sobra da divisão por 2 é igual a 0, ou seja se esse número é par.
+    Caso seja par ele armazena na array1 se for diferente de par, ou seja, impar, ele armazena na array2.
+    No fim ele imprime no console:
+
+    Quantidade total de números 14
+    6
+    8
+*/
