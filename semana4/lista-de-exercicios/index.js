caa5937d962e9af939eb568a4ec8fefd0ee9c29a
Augusto Luna <caugustolunas@gmail.com>
2020-10-12 14:51:18 -0300

    Exercicio 5 de funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -436,3 +436,27 @@ console.log (enviaEmail)
 
 
 
+//Exercício 5
+
+/*
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+const contasAtualizadas = contas.forEach((elemento) => {
+    let soma = 0
+
+    for (let compra of elemento.compras) {
+        soma += compra
+    }
+
+    elemento.saldoTotal -= soma
+
+})
+console.log(contas)
+*/
