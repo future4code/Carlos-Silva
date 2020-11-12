4ac8438cdd8e0ab0bc8e5aca72ac62cf2945eb9e
Augusto Luna <caugustolunas@gmail.com>
2020-11-12 16:28:49 -0300

    projeto astromatch completo

--- semana9/projeto-astromatch/src/App.js
+++ semana9/projeto-astromatch/src/App.js
@@ -1,12 +1,29 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import styled from "styled-components"
 import axios from "axios"
 import MainPage from "./components/MainPage"
+import ListPage from "./components/ListPage"
+
+
 
 const App = (props) => {
+  const [renderMainPage, setRenderMainPage] = useState(true)
+  const [renderListPage, setRenderListPage] = useState(false)
+
+  const renderedPage = (page) => {
+    if(page === "MainPage"){
+      setRenderMainPage(true)
+      setRenderListPage(false)
+    }else{
+      setRenderMainPage(false)
+      setRenderListPage(true)
+    }
+  }
+
   return (
     <div>
-      <MainPage />
+      {renderMainPage && <MainPage renderMain={renderedPage} renderList={renderedPage}/>}
+      {renderListPage && <ListPage renderMain={renderedPage} renderList={renderedPage}/>}
     </div>
   );
 }
