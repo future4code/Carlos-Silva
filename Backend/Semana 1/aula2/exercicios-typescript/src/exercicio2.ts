08ac344ecb7b25f7cb401cc6eae2bc4ab6f6b80c
Augusto Luna <caugustolunas@gmail.com>
2021-01-05 18:00:29 -0300

    Exercicio 2 terminado - iniciando o 3

--- Backend/Semana 1/aula2/exercicios-typescript/src/exercicio2.ts
+++ Backend/Semana 1/aula2/exercicios-typescript/src/exercicio2.ts
@@ -1,22 +1,53 @@
-//*** Letra a ***
+//*** Letra a e letra b***
 
-function obterEstatisticas(numeros) {
+// A entrada é o parametro da função e a saída é o return.
 
-    const numerosOrdenados = numeros.sort(
+/*
+function obterEstatisticas(numeros: number[]) {
+
+    const numerosOrdenados: number[] = numeros.sort(
         (a, b) => a - b
     )
 
-    let soma = 0
+    let soma: number = 0
 
     for (let num of numeros) {
         soma += num
     }
 
-    const estatisticas = {
+    const estatisticas: any = {
         maior: numerosOrdenados[numeros.length - 1],
         menor: numerosOrdenados[0],
         media: soma / numeros.length
     }
 
     return estatisticas
+}
+
+*/
+
+//*** Letra c***
+
+type amostraDeDados = {
+    numeros: [5,10,15,20,25,30],
+    obterEstatisticas: (numeros: number[]) => {
+
+        const numerosOrdenados = numeros.sort(
+            (a, b) => a - b
+        )
+    
+        let soma: number = 0
+    
+        for (let num of numeros) {
+            soma += num
+        }
+    
+        const estatisticas: any = {
+            maior: numerosOrdenados[numeros.length - 1],
+            menor: numerosOrdenados[0],
+            media: soma / numeros.length
+        }
+    
+        return estatisticas
+    }
 }
